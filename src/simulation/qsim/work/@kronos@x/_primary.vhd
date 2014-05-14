089e1504a1a24724fc104090d164351c330b1900
ignitz <ignitzhjfk@gmail.com>
2014-05-13 22:05:10 -0300

    Arquivos iniciais
    Documentação do trabalho prático e código inicial para teste

---
+++ src/simulation/qsim/work/@kronos@x/_primary.vhd
@@ -0,0 +1,11 @@
+library verilog;
+use verilog.vl_types.all;
+entity KronosX is
+    port(
+        pin_name5       : out    vl_logic;
+        pin_name1       : in     vl_logic;
+        pin_name2       : in     vl_logic;
+        pin_name3       : in     vl_logic;
+        pin_name4       : in     vl_logic
+    );
+end KronosX;
---
+++ src/simulation/qsim/work/@kronos@x_vlg_vec_tst/_primary.vhd
@@ -0,0 +1,4 @@
+library verilog;
+use verilog.vl_types.all;
+entity KronosX_vlg_vec_tst is
+end KronosX_vlg_vec_tst;
