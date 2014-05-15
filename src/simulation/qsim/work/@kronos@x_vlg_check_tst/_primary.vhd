7db10de9984e98dc6b5f0911b70e719521e4cb94
ignitz <ignitzhjfk@gmail.com>
2014-05-14 23:05:19 -0300

    Iniciando o arquivo da ALU
    Uma pasta por item, inserido o projeto do ALU

--- src/simulation/qsim/work/@kronos@x_vlg_check_tst/_primary.vhd
+++
@@ -1,8 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity KronosX_vlg_check_tst is
-    port(
-        pin_name5       : in     vl_logic;
-        sampler_rx      : in     vl_logic
-    );
-end KronosX_vlg_check_tst;
--- src/simulation/qsim/work/@kronos@x_vlg_vec_tst/_primary.vhd
+++
@@ -1,4 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity KronosX_vlg_vec_tst is
-end KronosX_vlg_vec_tst;
